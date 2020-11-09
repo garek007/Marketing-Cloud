bd5326669585495c0602c362f42d734f66a6f62e
Stan Alachniewicz <stan_alach@hotmail.com>
2020-11-09 15:04:58 -0500

    Update 01_Individual_Emails_Sent_Last_Month_SDV.sql
    updated formatting

--- custom-report-monthly-kpis/sql/01_Individual_Emails_Sent_Last_Month_SDV.sql
+++ custom-report-monthly-kpis/sql/01_Individual_Emails_Sent_Last_Month_SDV.sql
@@ -1,4 +1,14 @@
-SELECT s.SubscriberKey,s.SubscriberID,j.EmailName,s.JobID,s.ListID,s.BatchID,s.TriggeredSendCustomerKey,s.Domain,s.EventDate,CONVERT(VARCHAR(7),s.EventDate,111) as YearMonth
+SELECT 
+  s.SubscriberKey
+, s.SubscriberID
+, j.EmailName
+, s.JobID
+, s.ListID
+, s.BatchID
+, s.TriggeredSendCustomerKey
+, s.Domain
+, s.EventDate
+, CONVERT(VARCHAR(7),s.EventDate,111) as YearMonth
 FROM _Sent s
 JOIN _Job j
 ON s.JobID = j.JobID
